296146bdd3da0f16d11d106d34c0ade338d511ea
Marina <marfen24@i.ua>
2019-12-13 21:26:12 +0200

    Updates

---
+++ precache-manifest.96262ef0a9d8fbf4740ac3a5ebc578ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4019ba384ceefccfd729732807581a9",
+    "url": "/goit-react-hw-01-components/react_js_20-master/index.html"
+  },
+  {
+    "revision": "cd8f3117ef3b98f3b3c1",
+    "url": "/goit-react-hw-01-components/react_js_20-master/static/css/main.d43335bf.chunk.css"
+  },
+  {
+    "revision": "42641977c3b6bdfd94e6",
+    "url": "/goit-react-hw-01-components/react_js_20-master/static/js/2.e109b485.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-01-components/react_js_20-master/static/js/2.e109b485.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cd8f3117ef3b98f3b3c1",
+    "url": "/goit-react-hw-01-components/react_js_20-master/static/js/main.f89b089f.chunk.js"
+  },
+  {
+    "revision": "28f3ff1dc4845af9d6f3",
+    "url": "/goit-react-hw-01-components/react_js_20-master/static/js/runtime-main.5e13a83f.js"
+  }
+]);
